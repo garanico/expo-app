8c448bbc4b26eb834987d11742937b30a523e2ea
Grace Aranico <grace.aranico@gmail.com>
2021-09-29 17:30:12 -0700

    recreated profile project in expo

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import grace from './assets/grace.jpg';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={grace} style={styles.grace}/>
+      <Text style={styles.name}>Grace Aranico</Text>
+      <Text style={styles.title}>React Native Apprentice</Text>
+      <Text style={styles.pronoun}>She/Her</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +21,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    border: '2px solid gray',
+    width: 350,
+    margin: "auto",
+    
+    
   },
+  grace: {
+    width: 300,
+    height: 450,
+    borderRadius: 20,
+    border: '2px solid gray',
+  },
+  name: {
+    margin: 5,
+  },
+  title: {
+    margin: 5,
+  },
+  pronoun: {
+    margin: 5,
+  }
 });
